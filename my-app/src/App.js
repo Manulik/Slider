e0b0a4cc2b079ca5d77da66e897acfe429cb0753
Manulik <verstalchik05@mail.ru>
2019-09-04 17:38:19 +0300

    Add new files

---
+++ my-app/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  state = {
+    imgArr: [
+      'https://alfabank.com/app/uploads/2018/03/index_banner_1.jpg',
+      'https://alfabank.com/app/uploads/2018/09/Brand_1440_500_year_results.jpg',
+      'https://alfabank.com/app/uploads/2018/08/Brand_1440_500_fin_otchet.jpg'
+    ],
+    imgIndex: 0
+  };
+  handleVperedClick = () => {
+    this.setState({imgIndex: this.state.imgIndex + 1});
+  }
+  handleNazadClick = () => {
+    this.setState({imgIndex: this.state.imgIndex - 1});
+  }
+  render() {
+    return (
+      <div className="App">
+        <img src={this.state.imgArr[this.state.imgIndex]} />
+        <div className="buttonBlock">
+          {this.state.imgIndex !== 0 && <button className="left" onClick={this.handleNazadClick}></button>}
+          {this.state.imgIndex !== this.state.imgArr.length - 1 && <button className="right" onClick={this.handleVperedClick}></button>}
+        </div>
+        <div className='circes'>
+          this.state.imgArr.map((img, index) => (
+            <div key={img} className={index === this.state.imgIndex ? 'blackCircle' : 'circle'}></div>
+          ))
+        </div>
+      </div>
+    )
+  }
+}
+export default App
